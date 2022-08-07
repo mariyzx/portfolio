306090d4ab64062ae4e79f6923e1ccd48b72ce38
Mariana Werneck <marinhomariana8@gmail.com>
2022-08-07 19:58:21 -0300

    Correção media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ class Header extends React.Component {
           <ul>
             <li onClick={(e) => {e.preventDefault(); window.location.replace('/#home')}}>HOME</li>
             <li onClick={(e) => {e.preventDefault(); window.location.replace('/#projetos')}}>PROJETOS</li>
-            <li onClick={(e) => {e.preventDefault(); window.location.replace('/#about')}}>SOBRE MIM</li>
+            <li onClick={(e) => {e.preventDefault(); window.location.replace('/#about')}}>SOBRE</li>
             <li onClick={(e) => {e.preventDefault(); window.location.replace('/#contato')}}>CONTATO</li>
           </ul>
         </nav>
