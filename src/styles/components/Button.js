35c187225d4002f9b5d6248f778cd75dc59debb2
Mariana Werneck <marinhomariana8@gmail.com>
2022-12-16 01:18:44 -0300

    Estilizando sessão de contato

--- src/styles/components/Button.js
+++ src/styles/components/Button.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   background: none;
-	color: inherit;
 	border: none;
 	padding: 0;
 	font: inherit;
---
+++ src/styles/components/Contact.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const MainContact = styled.section`
+  text-align: center;
+
+  div {
+    display: flex;
+    margin-top: 13px;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  form {
+    margin-top: 13px;
+    display: flex;
+    gap: 1rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 10%;
+    margin-top: 30px;
+  }
+
+
+  form > input, form > textarea {
+    max-width: 190px;
+    height: 44px;
+    background-color: #05060f0a;
+    border-radius: .5rem;
+    padding: 0 1rem;
+    border: 2px solid transparent;
+    font-size: 1rem;
+    resize: none;
+    transition: border-color .3s cubic-bezier(.25,.01,.25,1) 0s, color .3s cubic-bezier(.25,.01,.25,1) 0s,background .2s cubic-bezier(.25,.01,.25,1) 0s;
+  }
+
+  form > input:focus {
+    border-color: purple;
+    outline: none;
+  }
+
+`
+
+export const DivContact = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+`
