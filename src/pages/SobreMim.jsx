62e0fffcccccc44227f6e4d12c7e406e79a403e3
Mariana Werneck <marinhomariana8@gmail.com>
2022-12-22 14:27:42 -0300

    Ajuste no sobre mim

--- src/pages/SobreMim.jsx
+++ src/pages/SobreMim.jsx
@@ -8,11 +8,11 @@ class SobreMim extends React.Component {
         <h2>Sobre mim</h2>
         <div>
           <article className="about_card">
-            21 anos, carioca, atualmente aprendendo desenvolvimento back-end na Trybe!
+            21 anos, carioca, atualmente aprendendo desenvolvimento fullstack na Trybe!
             <br></br>
             Amo café, ficção científica, passar meu tempo livre jogando e aprender coisas novas!
             <br></br>
-            Em busca da minha oportunidade como dev ♥
+            Em busca da minha primeira oportunidade como dev ♥
           </article>
           <a href="/#contato" className="btn btn-primary">Vamos conversar ✉</a>
         </div>
