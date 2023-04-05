2fb026f78bda823852c80d40f5bf496ea627350d
Mariana <marinhomariana8@gmail.com>
2023-04-05 19:32:14 -0300

    :sparkles: feat: add span e link para contato

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,11 @@ class Main extends React.Component {
   render() {
     return (
       <MainTitle>
-        <h3>Portfolio</h3>
-        <h1>Mariana Werneck</h1>
-        <h3>Front-end developer</h3>
+        <span>
+          <h1>Mariana Werneck</h1>
+          <h2>Front-end developer</h2>
+        </span>
+        <a href="/#contato" className="btn btn-primary">Vamos conversar ✉</a>
       </MainTitle>
     );
   }
