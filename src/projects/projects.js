fd25e6dc8143bea7fe0b965ff6786408c0f5c2c2
Mariana Werneck <marinhomariana8@gmail.com>
2023-05-29 15:13:01 -0300

    :sparkles: feat: adiciona songify, trybeer e car shop

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,38 +1,36 @@
-const trybetunes = require('../assets/thumbs/trybetunes.PNG');
-const trybewallet = require('../assets/thumbs/trybewallet3.png');
+const songify = require('../assets/thumbs/songify.png');
+const trybeer = require('../assets/thumbs/trybeer.jpg');
 const recipes = require('../assets/thumbs/recipes-app.png');
 const tfc = require('../assets/thumbs/tfc.PNG');
+const car_shop = require('../assets/thumbs/car_shop.png');
 
 module.exports = [
   {
-    name: 'Trybetunes',
-    img: trybetunes,
+    name: 'Songify',
+    img: songify,
     links: {
-      preview: 'https://trybe-trybetunes.vercel.app/',
-      github: 'https://github.com/mariyzx/trybe-trybetunes',
+      github: 'https://github.com/mariyzx/songify',
     },
-    description: 'Aplicação de busca de músicas e álbuns, é possível favoritar uma faixa específica do álbum, editar perfil e verificar as faixas favoritadas.',
+    description: 'Aplicação full-stack de busca de músicas e álbuns, é possível favoritar uma faixa específica do álbum, editar perfil e verificar as faixas favoritadas.',
     tech: ['ReactJS', 'CSS']
-  },
-  {
-    name: 'Trybewallet',
-    img: trybewallet,
-    links: {
-      preview: 'https://trybe-trybewallet-nu.vercel.app/',
-      github: 'https://github.com/mariyzx/trybe-trybewallet',
-    },
-    description: 'Aplicação que computa gastos em diferentes moedas e converte tudo para uma única moeda, utilizando uma API de Cotação de Moedas.',
-    tech: ['Redux', 'ReactJS', 'CSS', 'API', 'RTL']
   },
   {
     name: 'Recipes App',
     img: recipes,
     links: {
-      preview: 'http://projeto-receitas.vercel.app/',
-      github: 'https://github.com/mariyzx/projeto-receitas',
+      github: 'https://github.com/mariyzx/Recipes-App',
     },
-    description: 'Projeto final de Front-End! Aplicação que retorna receitas de comidas e bebidas, com filtros, tela de receitas favoritadas e receitas concluídas.',
+    description: 'Projeto final de front-end! Aplicação que retorna receitas de comidas e bebidas, com filtros, tela de receitas favoritadas e receitas concluídas.',
     tech: ['ReactJS', 'CSS', 'Context API', 'Styled Components']
+  },
+  {
+    name: 'Trybeer',
+    img: trybeer,
+    links: {
+      github: 'https://github.com/mariyzx/trybeer',
+    },
+    description: 'Projeto final de back-end! Aplicação full-stack de delivery de cerveja, onde é possível logar tanto como usuário ou vendedor.',
+    tech: ['ReactJS', 'MySQL', 'Node.js', 'API', 'RTL']
   },
   {
     name: 'TFC',
@@ -41,7 +39,16 @@ module.exports = [
       preview: 'https://tfc-mariyzx.vercel.app/leaderboard',
       github: 'https://github.com/mariyzx/tfc',
     },
-    description: 'API para um site informativo de partidas e classificações de futebol, com listagem de times, partidas e autenticação do usuário. API documentada com o Swagger.',
+    description: 'API para um site informativo de partidas e classificações de futebol, com listagem de times, partidas e autenticação do usuário.',
     tech: ['Node.js', 'MySQL', 'Docker', 'Typescript', 'Sequelize']
   },
+  {
+    name: 'Car Shop',
+    img: car_shop,
+    links: {
+      github: 'https://github.com/mariyzx/car-shop'
+    },
+    description: 'API para gerenciar uma concessionária de veículos, utilizando o banco de dados MongoDB através do framework Mongoose.',
+    tech: ['Typescript', 'MongoDB', 'Mongoose', 'API']
+  }
 ]
