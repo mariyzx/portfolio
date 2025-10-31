b33a15a568550cdf3fd4b2640cd33d6b4d158913
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-31 16:24:35 -0300

    feat: refactor App component to use React Router and React Query

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+     <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+};
+
+export default Index;
